be1020b44fd28cd065da0d4c0135764b70812211
Manuel <manueldrei@gmail.com>
2022-10-05 23:37:59 +0200

    ok

---
+++ operatori(libro).js
@@ -0,0 +1,14 @@
+/*
+lista operatori
++
+-
+/
+*
+++ (incremento)
+-- (decremento)
+% (resto)
+*/
+let pOp = "ciao";
+let pIp = "Buondì";
+console.log(pOp + pIp); //Fa una concatenazione, quindi visualizzo ciao boundì
+
