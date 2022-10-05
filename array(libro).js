7d0620528d922efc715acc86e3cc05ebcb6aca21
Manuel <manueldrei@gmail.com>
2022-10-05 23:29:41 +0200

    ok

---
+++ array(libro).js
@@ -0,0 +1,7 @@
+let colors;
+colors= ['green', 'white', 'black']; //index array è 0=green, 1=white, 2=black
+console.log(colors[2]); //Verrà visualizzato il colore black
+let numColors;
+numColors=colors.length;
+console.log(numColors); //Verrà visualizzata la lunghezza dell'array cioè 3
+colors[2]='beige'; //Aggiorna il valore dell'index 2 dell'array trasformandolo da black a beige
