bef7928839a6b95358c6da5f381b2afe2384288a
manudrei <107146298+manudrei@users.noreply.github.com>
2022-10-06 09:24:14 +0200

    ok

--- operatori(libro).js
+++ operatori(libro).js
@@ -7,8 +7,18 @@ lista operatori
 ++ (incremento)
 -- (decremento)
 % (resto)
+=== (operatore di comprazione)
+!== (operatore di diversità)
+>
+<
+<=
+>=
 */
+
+
 let pOp = "ciao";
 let pIp = "Buondì";
 console.log(pOp + pIp); //Fa una concatenazione, quindi visualizzo ciao boundì
 
+console.log(10 === 11); //=== confronta i due numeri,  e risponde a schermo true o false. In questo caso sarà un false perchè i numeri sono diversi
+console.log(10 !== 11); //In questo caso la risposta è true perchè i numeri sono diversi
