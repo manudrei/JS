1dff21ee6305496edbe9c82ab19b1b24d5d64382
manudrei <107146298+manudrei@users.noreply.github.com>
2022-10-06 18:18:15 +0200

    ok

--- operatori(libro).js
+++ operatori(libro).js
@@ -22,3 +22,12 @@ console.log(pOp + pIp); //Fa una concatenazione, quindi visualizzo ciao boundì
 
 console.log(10 === 11); //=== confronta i due numeri,  e risponde a schermo true o false. In questo caso sarà un false perchè i numeri sono diversi
 console.log(10 !== 11); //In questo caso la risposta è true perchè i numeri sono diversi
+
+//Se faccio un addizione tra una stringa e un numero o boolean riceverò sempre una stringa come risultato
+
+/* OPERTATORI LOGICI
+
+&& AND
+|| OR
+! NOT
+*/
