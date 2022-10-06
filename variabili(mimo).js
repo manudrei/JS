89269760c0484b6c6921c85d1b6be80142fa77d5
manudrei <107146298+manudrei@users.noreply.github.com>
2022-10-06 08:58:26 +0200

    ok

---
+++ variabili(mimo).js
@@ -0,0 +1,17 @@
+let city; // let e const vengono utilizzati per dichiarare le variabili, la variabile deve essere senza spazi, può essere sia con maiuscole e minuscole
+const topolino = 3; //const viene usato per variabili che rimarranno costanti
+const cities = "Chicago"; //Si usa l'uguale per impostare un valore
+console.log(); //Si usa per visualizzare a schermo il valore di una variabile o una stringa
+
+const numberOfFollowers = "55"; //Le doppie virgolette trattano la variabile come una stringa
+console.log("Followers:" + numberOfFollowers)  ; //In questo caso quello visualizzato è Followers:55
+
+const savings = 100;
+const interest = 0.04;
+console.log(savings * interest); //in questo caso vedo il risultato dell'espressione perchè non ci sono le virgolette
+
+const paperino = true; //alle variabili o costanti posso dare il valore true o false
+const paperina = false;
+
+const isMorning = true;
+console.log(!isMorning); //l'operatore ! è l'operatore negazione, in questo caso vedo a schermo false perchè nega il true
