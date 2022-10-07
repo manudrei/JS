c8d6aafe74bd056576dc94261dd247f59710dcac
manudrei <107146298+manudrei@users.noreply.github.com>
2022-10-07 11:00:06 +0200

    ok

--- condizionali(mimo).js
+++ condizionali(mimo).js
@@ -1,3 +1,11 @@
 if (true) { //Se la condizione è vera fa vedere a schermo 3 2 1 GO invece se falsa lo salta
     console.log("3 2 1 GO");
    }
+
+   let condition = false; //In questo caso è falsa e eseguirà l'istruzione else
+   if (condition) {
+    console.log("1");
+   }  
+   else{
+    console.log("2");
+   } 
