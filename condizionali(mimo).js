0249598d062bc2521d69166c5d9e3feca1c45d82
manudrei <107146298+manudrei@users.noreply.github.com>
2022-10-07 12:24:32 +0200

    ok

--- condizionali(mimo).js
+++ condizionali(mimo).js
@@ -1,7 +1,9 @@
+/*IF*/
 if (true) { //Se la condizione è vera fa vedere a schermo 3 2 1 GO invece se falsa lo salta
     console.log("3 2 1 GO");
    }
 
+  /* IF ELSE */
    let condition = false; //In questo caso è falsa e eseguirà l'istruzione else
    if (condition) {
     console.log("1");
@@ -9,3 +11,11 @@ if (true) { //Se la condizione è vera fa vedere a schermo 3 2 1 GO invece se fa
    else{
     console.log("2");
    } 
+
+/*ELSE IF STATEMENT */
+let hour = 14;
+if (hour < 12) { //Se è falsa passa avanti
+ console.log("Good morning");
+} else if (hour < 17) { //Se è vera fa vedere console.log se invece falsa lo salta
+ console.log("Good afternoon");
+}
