ea96726882d0507f2ff2e721054d5e77092ba14c
manudrei <107146298+manudrei@users.noreply.github.com>
2022-10-11 18:13:16 +0200

    ok

--- loop.js
+++ loop.js
@@ -2,3 +2,7 @@ while (true) //Il ciclo while si loopa finchè è true, usce solo quando la cond
 {
     console.log ("Hi there!");
 }
+
+for (let i = 0; i < 4; i++) { //i=0 è la condizione iniziale, poi li si loopa finchè i è minore di 4
+    console.log("-------------------");
+   }
