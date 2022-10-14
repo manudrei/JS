76cb6dd5f35ed0926db23c65397b17d8a302dde2
manudrei <107146298+manudrei@users.noreply.github.com>
2022-10-14 12:29:38 +0200

    js

--- functions.js
+++ functions.js
@@ -2,3 +2,10 @@ function(ciAo){     //Come si dichiare una funzione
 
 
 }
+
+
+function displayWinners(first, second) { //Le funzioni servono per richiamare alcune operazioni tanto usate
+    console.log("1st: " + first);
+    console.log("2nd: " + second);
+   }
+   displayWinners("Amy","Sam");
