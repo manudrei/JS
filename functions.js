3334a2fb533da23dc983d49f452a6ef7b69c526c
manudrei <107146298+manudrei@users.noreply.github.com>
2022-10-19 18:28:31 +0200

    ok

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-function(ciAo){     //Come si dichiare una funzione
+function(Ciao){     //Come si dichiare una funzione
 
 
 }
@@ -9,3 +9,21 @@ function displayWinners(first, second) { //Le funzioni servono per richiamare al
     console.log("2nd: " + second);
    }
    displayWinners("Amy","Sam");
+
+
+function isRentingAge(age) //Funzione con dentro un if
+{
+if (age >= 21) 
+{
+console.log("Approved");
+ }
+}
+isRentingAge(26);
+
+function isLying(boolean) {
+    console.log("Welcome to the Lie Detector 3000");
+    if (boolean === false) {
+     console.log("He's lying");
+    }
+   }
+   isLying(false);
