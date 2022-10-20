7e396748475a902299cc87710fdc7cb7138fe49a
manudrei <107146298+manudrei@users.noreply.github.com>
2022-10-20 11:47:06 +0200

    ok

--- functions.js
+++ functions.js
@@ -27,3 +27,55 @@ function isLying(boolean) {
     }
    }
    isLying(false);
+   
+   
+   
+   
+   function multiplyByTen(number) { //il tipo di variabile è un number
+    return number * 10; //Ritorna il risultato moltiplicato 10
+    console.log("This won't get executed"); //Qualsiasi cosa dopo un return non viene eseguito
+    }
+    const result = multiplyByTen(29); 
+    console.log(result); //risultato è 290
+
+    function getDiscount(age) {
+        if (age >= 65) 
+        {
+         return "50% OFF"; 
+        }
+        return "0% OFF";
+       }
+       const discount = getDiscount(67);
+       console.log(discount); //essendo discount maggiore di 65 visualizza a schermo 50% OFF
+    
+       function getDiscount(age) {
+        if (age <= 18) {
+         return "50% OFF";
+        } else if (age >= 65) {
+         return "30%";
+        }
+        
+       return "Full price";
+       
+       }
+       
+       const discount1 = getDiscount(25); //in questo caso visualizziamo full price
+       console.log(discount1);
+
+       
+       
+       
+       function displayPageNumbers(page) {
+        for (let i = 1; i <= 20; i++) {
+         console.log(page+i); //visualizziamo la parola page seguito da un numero da 1 a 20
+        }
+       }
+       displayPageNumbers("Page ");
+
+       function displayYears(start, end) {
+        for (let i = start; i <= end; i++) { //abbiamo due variabili start e end
+         console.log(i);
+        }
+       }
+       
+       displayYears(1998,2000);
